576b3c9bcac6ed59c32e03ebc77df0637acef422
cynthia1004 <128720734+cynthia1004@users.noreply.github.com>
2023-04-11 10:06:41 -0500

    lab 7.py

---
+++ lab - lecture 7.py
@@ -0,0 +1,32 @@
+# 1. Will the code run, & if so, what will be the data types & values of a & b?
+a, b = [10, 11]
+a, *b = [10]
+a, *b = [10, 11, 12]
+a, *b = [10, 11]
+a, *b = [10]
+a, *b = [10, 11, 12]
+
+
+# 2. What data type is args & kwargs inside the function, what are the values,
+# and how would you use them?
+def base_function(*args, **kwargs):
+    pass
+
+base_function()
+base_function(['A', 'B'])
+base_function('Hello', 'World', '!')
+base_function(answer=True, question='No')
+base_function('a', 'b', c='value', d=10)
+
+
+# 3. Write a lambda function that is passed into my_func, & performs a valid
+# operation on a & b, without editing the contents of my_func at all.
+
+def my_func(a, b, func):
+    value = func(a, b)
+    return value
+
+my_func = lambda a, b: func(a,b)
+
+
+my_func = lambda a: a+10
